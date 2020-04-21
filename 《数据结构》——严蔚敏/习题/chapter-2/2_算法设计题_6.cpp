2063da993b4886eed11c2347e490604bfb30c228
Gao-June <53166605+Gao-June@users.noreply.github.com>
2020-04-21 15:26:16 +0800

    Update 2_算法设计题_6.cpp

--- 《数据结构》——严蔚敏/习题/chapter-2/2_算法设计题_6.cpp
+++ 《数据结构》——严蔚敏/习题/chapter-2/2_算法设计题_6.cpp
@@ -1,24 +1,28 @@
 /*
-    设计一个算法，将链表中所有结点的链接方向“原地”逆转，即要求仅利用原表的存储空间，换句话说，
-    要求算法空间复杂度为O(1)
+    设计一个算法，通过一趟遍历，确定长度为n的单链表中值最大的结点
 */
-//还有一种写法，每次都将结点插在头结点之后
-void Overturn(L *a)
-{
-// p为当前节点， pre为它前面的一个节点
-    L *pre = new L; pre=a->next;
-    L *p = new L; p=pre->next;
-    L *temp = new L;
-//头结点变成了最后一个
-    pre->next=NULL;
 
-    while(p)
+int Find_max(L *a, int n)
+{
+    int num=1;
+//初始情况p指向第一个，temp指向第二个位置
+    L *p = new L; p=a->next;
+    L *temp = new L; temp = p->next;
+
+    for(int i=2; i<=n; i++)
     {
-        temp = p;
-        p=p->next;
-        temp->next = pre;
-        pre = tmp;
+        if(p->data < temp->data)
+        {
+            num = i;
+            p = temp;
+        }
+        temp = temp->next;
     }
-    a->next=pre;
+    return num;
 }
+
+
+
+
+
 
